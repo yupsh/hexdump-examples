ef9cb4766e988bff748bc087f39122c08aba4a85
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:50:56 -0800

    tests

--- basic_test.go
+++ basic_test.go
@@ -3,17 +3,15 @@ package hexdump_test
 import (
 	"strings"
 
-	yup "github.com/gloo-foo/framework"
-	. "github.com/yupsh/hexdump"
+	gloo "github.com/gloo-foo/framework"
 )
 
 func ExampleHexdump_basic() {
 	// echo "Hello" | hexdump
-	yup.MustRun(
+	gloo.MustRun(
 		Hexdump(strings.NewReader("Hello")),
 	)
 	// Output:
 	// 00000000  48 65 6c 6c 6f                                    |Hello|
 	// 00000005
 }
-
--- fromfile_basic_test.go
+++ fromfile_basic_test.go
@@ -1,18 +1,17 @@
 package hexdump_test
 
 import (
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/hexdump"
 )
 
 // This example demonstrates reading from a file instead of strings.NewReader
 func ExampleHexdump_fromFile_basic() {
 	// hexdump testdata/binary.txt
-	yup.MustRun(
-		Hexdump(yup.File("testdata/binary.txt")),
+	gloo.MustRun(
+		Hexdump(gloo.File("testdata/binary.txt")),
 	)
 	// Output:
 	// 00000000  48 65 6c 6c 6f                                    |Hello|
 	// 00000005
 }
-
